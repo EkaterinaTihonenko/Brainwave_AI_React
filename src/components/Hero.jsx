962ade52ea60febc6a7916eae9115cf601142ed4
Ekaterina <ekaterinatihonenko8@gmail.com>
2024-03-17 06:55:25 +0300

    main

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,8 +23,8 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Исследуйте возможности&nbsp;Искусственного интеллекта&nbsp;Общаясь с{" "}
-            {` `}
+            Исследуйте возможности Искусственного&nbsp; интеллекта&nbsp;Общаясь
+            с {` `}
             <span className="inline-block relative">
               Brainwave{" "}
               <img
--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Brainwave_AI_React",
   plugins: [react()],
 });
